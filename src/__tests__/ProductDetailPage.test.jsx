c6fbf3f9a74cd30f70e32ba54b5f030784e1da50
felicien <felicienkibet@gmail.com>
2026-05-14 23:34:00 +0300

    Add useProducts hook tests and fix jest-dom imports for Vitest

--- src/__tests__/ProductDetailPage.test.jsx
+++ src/__tests__/ProductDetailPage.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
+import '@testing-library/jest-dom';
 import { MemoryRouter, Route } from 'react-router-dom';
 import ProductDetailPage from '../pages/ProductDetailPage';
 
